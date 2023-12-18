920caaa52da9fa9a81545fbc4b8ca9ff478c3b7c
technologicaljerry <cserajatverma@gmail.com>
2023-12-18 21:13:26 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,12 @@ import bodyParser from "body-parser";
 
 const app = express();
 
-const HOST = 'localhost';
+const HOST = "localhost";
 
 const PORT = 5050;
 
 app.use(bodyParser.json());
 
 app.listen(PORT, () => {
-    console.log(`server is listening on http://${HOST}:${PORT}`);
-  });
+  console.log(`server is listening on http://${HOST}:${PORT}`);
+});
